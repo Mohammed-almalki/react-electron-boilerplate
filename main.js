37661f2d6eaa12e4cec6a0d7039e42f917c6b87b
Mohammed Almalki <mr.mhmd.almalki@gmail.com>
2025-03-15 17:37:08 +0300

    Initialize React Boilerplate for Electron applications with essential configurations and files

---
+++ main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, Menu, session, globalShortcut } = require('electron')
+const path = require('path')
+
+
+isDev = true;
+
+
+function rootDir(file) {
+    return path.join(__dirname, file)
+}
+
+
+function createWindow() {
+    Menu.setApplicationMenu(null);
+    const mainWindow = new BrowserWindow({
+        icon: rootDir('build/icon.png'),
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: rootDir('preload/shared.js'),
+        },
+    });
+
+
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                ...details.responseHeaders,
+                'Content-Security-Policy': ["default-src 'self'"]
+            }
+        })
+    });
+
+
+    if (isDev) {
+        mainWindow.loadURL("http://localhost:5173");
+        globalShortcut.register("F2", () => {
+            mainWindow.webContents.openDevTools();
+        });
+    } else {
+        mainWindow.loadFile(rootDir("/dist/index.html"));
+    }
+}
+
+
+app.whenReady().then(() => {
+    createWindow();
+})
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+
+app.on('web-contents-created', (event, contents) => {
+    contents.setWindowOpenHandler(({ url }) => {
+        return { action: 'deny' }
+    })
+})
